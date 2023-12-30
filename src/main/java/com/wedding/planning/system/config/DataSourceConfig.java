ebaffb73b49e63ace2a9a39e2ced3100b9a00acc
Smit Joshi <smitjoshi814@gmail.com>
2023-12-30 11:24:38 +0530

    db Config

--- src/main/java/com/wedding/planning/system/config/DataSourceConfig.java
+++ src/main/java/com/wedding/planning/system/config/DataSourceConfig.java
@@ -2,26 +2,27 @@ package com.wedding.planning.system.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DataSourceConfig {
+	@Value("${spring.datasource.url:}")
 	private final String url = env("SPRING_DATASOURCE_URL");
+	@Value("${spring.datasource.username:}")
 	private final String username = env("SPRING_DATASOURCE_USERNAME");
+	@Value("${spring.datasource.password:}")
 	private final String password = env("SPRING_DATASOURCE_PASSWORD");
 
+	
 	private static String env(String name) {
 		return System.getenv(name);
 	}
 
 	@Bean
 	DataSource dataSource() {
-		return DataSourceBuilder.create()
-				.url(url)
-				.username(username)
-				.password(password)
-				.build();
+		return DataSourceBuilder.create().url(url).username(username).password(password).build();
 	}
 }
