9bb663058a9ade4f0d2fe217bce2fdf20c8cd09e
Smit Joshi <smitjoshi814@gmail.com>
2023-12-30 09:26:44 +0530

    yml update

--- src/main/java/com/wedding/planning/system/config/DataSourceConfig.java
+++ src/main/java/com/wedding/planning/system/config/DataSourceConfig.java
@@ -8,20 +8,20 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DataSourceConfig {
-	 private final String url = env("SPRING_DATASOURCE_URL");
-	    private final String username = env("SPRING_DATASOURCE_USERNAME");
-	    private final String password = env("SPRING_DATASOURCE_PASSWORD");
+	private final String url = env("SPRING_DATASOURCE_URL");
+	private final String username = env("SPRING_DATASOURCE_USERNAME");
+	private final String password = env("SPRING_DATASOURCE_PASSWORD");
 
-	    private static String env(String name) {
-	        return System.getenv(name);
-	    }
+	private static String env(String name) {
+		return System.getenv(name);
+	}
 
-    @Bean
-    DataSource dataSource() {
-	        return DataSourceBuilder.create()
-	                .url(url)
-	                .username(username)
-	                .password(password)
-	                .build();
-	    }
+	@Bean
+	DataSource dataSource() {
+		return DataSourceBuilder.create()
+				.url(url)
+				.username(username)
+				.password(password)
+				.build();
+	}
 }
