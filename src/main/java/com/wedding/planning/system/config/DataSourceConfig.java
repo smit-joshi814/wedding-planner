a0b05c4ec455cf3ce344d223a3676f1f8b65bdd0
Smit Joshi <smitjoshi814@gmail.com>
2023-12-30 09:06:37 +0530

    db Configuration & v0.1

---
+++ src/main/java/com/wedding/planning/system/config/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package com.wedding.planning.system.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+	 private final String url = env("SPRING_DATASOURCE_URL");
+	    private final String username = env("SPRING_DATASOURCE_USERNAME");
+	    private final String password = env("SPRING_DATASOURCE_PASSWORD");
+
+	    private static String env(String name) {
+	        return System.getenv(name);
+	    }
+
+    @Bean
+    DataSource dataSource() {
+	        return DataSourceBuilder.create()
+	                .url(url)
+	                .username(username)
+	                .password(password)
+	                .build();
+	    }
+}
--- src/main/java/com/wedding/planning/system/config/RenderDeployes.java
+++ src/main/java/com/wedding/planning/system/config/RenderDeployes.java
@@ -1,13 +1,9 @@
 package com.wedding.planning.system.config;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
