cf9b4271ad83b3f7c8e5fa1c3b54b777b0111667
Smit Joshi <smitjoshi814@gmail.com>
2024-04-11 14:13:25 +0530

    cache config updated

--- src/main/java/com/wedding/planner/config/general/CahceConfig.java
+++ src/main/java/com/wedding/planner/config/general/CahceConfig.java
@@ -1,33 +1,10 @@
 package com.wedding.planner.config.general;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
-import com.wedding.planner.entity.Services;
-import com.wedding.planner.service.ServicesService;
-
-import jakarta.annotation.PostConstruct;
 
 @Configuration
 @EnableCaching
-public class CahceConfig {
-
-	@Autowired
-	private CacheManager cacheManager;
-	
-	@Autowired
-	private ServicesService serviceService;
-	
-	@PostConstruct
-	public void preloadCache() {
-		Cache cache = cacheManager.getCache("serviceCache");
-		List<Services> services = serviceService.getAll().getBody();
-		services.forEach(service-> cache.put(service.getServiceId(), service));
-	}
-	
+public class CahceConfig {	
 }
